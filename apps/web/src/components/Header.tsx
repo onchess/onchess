d9fe43a791cc0a5902a901ca44e762d5a009b264
Danilo Tuler <tuler@pobox.com>
2024-06-13 23:54:15 -0300

    Header link to future profile page

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,6 +1,14 @@
 "use client";
 
-import { ActionIcon, Anchor, Badge, Flex, Group, Text } from "@mantine/core";
+import {
+    ActionIcon,
+    Anchor,
+    Badge,
+    Flex,
+    Group,
+    Text,
+    useMantineTheme,
+} from "@mantine/core";
 import { Player, Token } from "@onchess/core";
 import {
     IconBrandGithub,
@@ -11,6 +19,7 @@ import {
     IconChessQueenFilled,
     IconChessRookFilled,
 } from "@tabler/icons-react";
+import Link from "next/link";
 import { FC } from "react";
 import { formatUnits } from "viem";
 
@@ -30,11 +39,16 @@ const chessIcons = [
 
 export const Header: FC<HeaderProps> = ({ player, token }) => {
     const { symbol } = token;
+
+    // choose a random wallet icon (just to be playful)
     const iconIndex = Math.min(
         Math.floor(Math.random() * chessIcons.length),
         chessIcons.length - 1,
     );
     const Icon = chessIcons[iconIndex];
+
+    const theme = useMantineTheme();
+
     return (
         <Group justify="space-between" py={10} px={20}>
             <Group align="baseline">
@@ -45,13 +59,20 @@ export const Header: FC<HeaderProps> = ({ player, token }) => {
             <Flex justify="flex-end" align="center" gap={5}>
                 <w3m-button />
                 {player && (
-                    <Badge
-                        size="xl"
-                        h={40}
-                        leftSection={<Icon color="white" size="1.5rem" />}
-                    >
-                        {formatUnits(BigInt(player.balance), 18)} {symbol}
-                    </Badge>
+                    <Link href="/profile">
+                        <Badge
+                            size="xl"
+                            h={40}
+                            bg={
+                                player.balance === "0"
+                                    ? theme.colors[theme.primaryColor][2]
+                                    : undefined
+                            }
+                            leftSection={<Icon color="white" size="1.5rem" />}
+                        >
+                            {formatUnits(BigInt(player.balance), 18)} {symbol}
+                        </Badge>
+                    </Link>
                 )}
                 <Anchor href="https://github.com/onchess" target="_blank">
                     <ActionIcon
--- apps/web/src/stories/Header.stories.ts
+++ apps/web/src/stories/Header.stories.ts
@@ -1,3 +1,4 @@
+import { INITIAL_RATING, Token } from "@onchess/core";
 import type { Meta, StoryObj } from "@storybook/react";
 import { Header } from "../components/Header";
 
@@ -10,14 +11,16 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+const token: Token = {
+    address: "0x92C6bcA388E99d6B304f1Af3c3Cd749Ff0b591e2",
+    decimals: 18,
+    name: "Test",
+    symbol: "TEST",
+};
+
 export const Disconnected: Story = {
     args: {
-        token: {
-            address: "0x92C6bcA388E99d6B304f1Af3c3Cd749Ff0b591e2",
-            decimals: 18,
-            name: "Test",
-            symbol: "TEST",
-        },
+        token,
     },
 };
 
@@ -25,18 +28,28 @@ export const Connected: Story = {
     args: {
         player: {
             address: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
-            balance: 350000000000000000000n.toString(),
+            balance: (350n * 10n ** BigInt(token.decimals)).toString(),
             played: 1,
             wins: 1,
             losses: 0,
             draws: 0,
             rating: 2310,
         },
-        token: {
-            address: "0x92C6bcA388E99d6B304f1Af3c3Cd749Ff0b591e2",
-            decimals: 18,
-            name: "Test",
-            symbol: "TEST",
+        token,
+    },
+};
+
+export const NoBalance: Story = {
+    args: {
+        player: {
+            address: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+            balance: "0",
+            played: 0,
+            wins: 0,
+            losses: 0,
+            draws: 0,
+            rating: INITIAL_RATING,
         },
+        token,
     },
 };
