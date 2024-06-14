df24ba93509bd17da4792a49d5568f9da0c44c51
Danilo Tuler <tuler@pobox.com>
2024-06-14 17:16:43 -0300

    Change rake to 2%

--- apps/web/src/stories/empty.ts
+++ apps/web/src/stories/empty.ts
@@ -5,7 +5,7 @@ export default {
     config: {
         eloKFactor: 20,
         owner: zeroAddress,
-        rakeDivider: 20,
+        rakeDivider: 50,
         token: {
             address: "0x92C6bcA388E99d6B304f1Af3c3Cd749Ff0b591e2",
             decimals: 18,
