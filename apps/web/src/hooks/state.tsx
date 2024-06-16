938c7fdd64ec8264172096305f25f08b4d9e0ee3
Danilo Tuler <tuler@pobox.com>
2024-06-16 00:49:23 -0300

    Fix state loader

--- apps/web/src/hooks/state.tsx
+++ apps/web/src/hooks/state.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@apollo/client";
 import { State } from "@onchess/core";
+import { useEffect, useState } from "react";
 import { Hex, hexToString } from "viem";
 import { gql } from "../__generated__";
 import { config } from "../providers/state";
@@ -34,37 +35,40 @@ export const useLatestState = (pollInterval: number = 2000): StateResponse => {
     const { data, loading, error } = useQuery(LATEST_STATE, {
         pollInterval,
     });
+    const [state, setState] = useState<State>({
+        config,
+        rake: "0",
+        games: {},
+        lobby: [],
+        players: {},
+        vouchers: [],
+    });
 
-    if (
-        data &&
-        data.inputs.edges.length > 0 &&
-        data.inputs.edges[0].node.notices.edges.length > 0
-    ) {
-        // hex string of notice
-        const hex = data.inputs.edges[0].node.notices.edges[0].node
-            .payload as Hex;
+    useEffect(() => {
+        if (
+            data &&
+            data.inputs.edges.length > 0 &&
+            data.inputs.edges[0].node.notices.edges.length > 0
+        ) {
+            // hex string of notice
+            const hex = data.inputs.edges[0].node.notices.edges[0].node
+                .payload as Hex;
 
-        // convert hex string string
-        const str = hexToString(hex);
+            // convert hex string string
+            const str = hexToString(hex);
 
-        if (str) {
-            // parse JSON
-            const obj = JSON.parse(str);
-            const state = obj.chess as State;
-            return { loading: false, state };
+            if (str) {
+                // parse JSON
+                const obj = JSON.parse(str);
+                const state = obj.chess as State;
+                setState(state);
+            }
         }
-    }
+    }, [data]);
 
     return {
         loading,
         error: error?.message,
-        state: {
-            config,
-            rake: "0",
-            games: {},
-            lobby: [],
-            players: {},
-            vouchers: [],
-        },
+        state,
     };
 };
