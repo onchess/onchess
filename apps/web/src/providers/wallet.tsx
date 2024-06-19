529ac049f08c375472582d3574b1c7dc2e1e41bc
Danilo Tuler <tuler@pobox.com>
2024-06-18 22:32:33 -0300

    Remove base mainnet for now

--- apps/web/src/providers/wallet.tsx
+++ apps/web/src/providers/wallet.tsx
@@ -5,7 +5,7 @@ import { createWeb3Modal } from "@web3modal/wagmi/react";
 import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
 import { FC, ReactNode } from "react";
 import { WagmiProvider } from "wagmi";
-import { base, baseSepolia, foundry } from "wagmi/chains";
+import { baseSepolia, foundry } from "wagmi/chains";
 
 const projectId = "2fc593e6b8e9da2434799b1111634ff0";
 const metadata = {
@@ -16,7 +16,7 @@ const metadata = {
 };
 
 export const config = defaultWagmiConfig({
-    chains: [base, baseSepolia, foundry],
+    chains: [baseSepolia, foundry],
     metadata,
     projectId,
     ssr: true,
