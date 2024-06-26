2ba0f1e1c0ae363c79f019d4bea973c2ad5cb6ab
Danilo Tuler <tuler@pobox.com>
2024-06-26 16:42:53 -0300

    Connect button

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,27 +1,9 @@
 "use client";
 
-import {
-    ActionIcon,
-    Anchor,
-    Badge,
-    Flex,
-    Group,
-    Text,
-    useMantineTheme,
-} from "@mantine/core";
+import { ActionIcon, Anchor, Flex, Group, Text } from "@mantine/core";
 import { Player, Token } from "@onchess/core";
-import {
-    IconBrandGithub,
-    IconBrandX,
-    IconChessBishopFilled,
-    IconChessKingFilled,
-    IconChessKnightFilled,
-    IconChessQueenFilled,
-    IconChessRookFilled,
-} from "@tabler/icons-react";
-import Link from "next/link";
-import { FC, useMemo } from "react";
-import { formatUnits } from "viem";
+import { IconBrandGithub, IconBrandX } from "@tabler/icons-react";
+import { FC } from "react";
 import { getProviderType } from "../providers/wallet";
 import { ConnectButton } from "./ConnectButton";
 
@@ -31,29 +13,8 @@ export type HeaderProps = {
     token?: Token;
 };
 
-const chessIcons = [
-    IconChessKingFilled,
-    IconChessBishopFilled,
-    IconChessKnightFilled,
-    IconChessQueenFilled,
-    IconChessRookFilled,
-];
-
 export const Header: FC<HeaderProps> = ({ player, token }) => {
     const provider = getProviderType();
-
-    // choose a random wallet icon (just to be playful)
-    const iconIndex = useMemo(
-        () =>
-            Math.min(
-                Math.floor(Math.random() * chessIcons.length),
-                chessIcons.length - 1,
-            ),
-        [],
-    );
-    const Icon = chessIcons[iconIndex];
-
-    const theme = useMantineTheme();
 
     return (
         <Group justify="space-between" py={10} px={20}>
@@ -64,25 +25,8 @@ export const Header: FC<HeaderProps> = ({ player, token }) => {
             </Group>
             <Flex justify="flex-end" align="center" gap={5}>
                 {provider === "WalletConnect" && <w3m-button />}
-                {provider === "ZeroDev" && <ConnectButton />}
-                {player && token && (
-                    <Link href="/bridge">
-                        <Badge
-                            size="xl"
-                            h={40}
-                            bg={
-                                player.balance === "0"
-                                    ? theme.colors[theme.primaryColor][2]
-                                    : undefined
-                            }
-                            leftSection={<Icon color="white" size="1.5rem" />}
-                        >
-                            {`${formatUnits(
-                                BigInt(player.balance),
-                                token.decimals,
-                            )} ${token.symbol}`}
-                        </Badge>
-                    </Link>
+                {token && provider === "ZeroDev" && (
+                    <ConnectButton token={token} balance={player?.balance} />
                 )}
                 <Anchor href="https://github.com/onchess" target="_blank">
                     <ActionIcon
