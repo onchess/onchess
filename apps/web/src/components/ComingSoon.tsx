abbefa3e5b22e290d9d0243b3b465d76634cc8f4
Danilo Tuler <tuler@pobox.com>
2025-08-01 18:33:09 -0400

    Fix home screen

--- apps/web/src/components/ComingSoon.tsx
+++ apps/web/src/components/ComingSoon.tsx
@@ -11,75 +11,79 @@ import {
 } from "@mantine/core";
 import { useViewportSize } from "@mantine/hooks";
 import { IconBrandGithub, IconBrandX } from "@tabler/icons-react";
-import { FC } from "react";
+import type { FC } from "react";
 
 export const ComingSoon: FC = () => {
     const { height } = useViewportSize();
-    return height > 0 ? (
-        <Stack h={height} align="stretch" justify="space-around">
-            <Center />
-            <Group align="baseline" justify="center">
-                <Image src="/img/knight.png" h={300} />
-                <Text ff="Cardo" fz={96} c="#273053" visibleFrom="md">
-                    OnChess
-                </Text>
-            </Group>
-            <Stack>
-                <Center>
-                    <Group gap={5}>
-                        <Text>coming on</Text>
-                        <Anchor href="https://base.org">
-                            <Image src="/img/base_icon.svg" h="20px" w="20px" />
-                        </Anchor>
-                        <Text>powered by</Text>
-                        <Anchor href="https://cartesi.io">
-                            <Image
-                                src="/img/cartesi_icon.svg"
-                                h="20px"
-                                w="20px"
-                            />
-                        </Anchor>
-                    </Group>
-                </Center>
-                <Center>
-                    <Group>
-                        <Anchor
-                            href="https://x.com/OnChessProject"
-                            target="_blank"
-                        >
-                            <ActionIcon
-                                size="lg"
-                                color="gray"
-                                variant="subtle"
-                                radius="lg"
-                            >
-                                <IconBrandX
-                                    style={{ width: "70%", height: "70%" }}
-                                    stroke={1.5}
+    return (
+        height > 0 && (
+            <Stack h={height} align="stretch" justify="space-around">
+                <Center />
+                <Group align="baseline" justify="center">
+                    <Image src="/img/knight.png" h={300} w={150} />
+                    <Text ff="Cardo" fz={96} c="#273053" visibleFrom="md">
+                        OnChess
+                    </Text>
+                </Group>
+                <Stack>
+                    <Center>
+                        <Group gap={5}>
+                            <Text>coming on</Text>
+                            <Anchor href="https://base.org">
+                                <Image
+                                    src="/img/base_icon.svg"
+                                    h="20px"
+                                    w="20px"
                                 />
-                            </ActionIcon>
-                        </Anchor>
-                        <Anchor
-                            href="https://github.com/onchess"
-                            target="_blank"
-                        >
-                            <ActionIcon
-                                size="lg"
-                                color="gray"
-                                variant="subtle"
-                                radius="lg"
-                            >
-                                <IconBrandGithub
-                                    style={{ width: "70%", height: "70%" }}
-                                    stroke={1.5}
+                            </Anchor>
+                            <Text>powered by</Text>
+                            <Anchor href="https://cartesi.io">
+                                <Image
+                                    src="/img/cartesi_icon.svg"
+                                    h="20px"
+                                    w="20px"
                                 />
-                            </ActionIcon>
-                        </Anchor>
-                    </Group>
-                </Center>
+                            </Anchor>
+                        </Group>
+                    </Center>
+                    <Center>
+                        <Group>
+                            <Anchor
+                                href="https://x.com/OnChessProject"
+                                target="_blank"
+                            >
+                                <ActionIcon
+                                    size="lg"
+                                    color="gray"
+                                    variant="subtle"
+                                    radius="lg"
+                                >
+                                    <IconBrandX
+                                        style={{ width: "70%", height: "70%" }}
+                                        stroke={1.5}
+                                    />
+                                </ActionIcon>
+                            </Anchor>
+                            <Anchor
+                                href="https://github.com/onchess"
+                                target="_blank"
+                            >
+                                <ActionIcon
+                                    size="lg"
+                                    color="gray"
+                                    variant="subtle"
+                                    radius="lg"
+                                >
+                                    <IconBrandGithub
+                                        style={{ width: "70%", height: "70%" }}
+                                        stroke={1.5}
+                                    />
+                                </ActionIcon>
+                            </Anchor>
+                        </Group>
+                    </Center>
+                </Stack>
             </Stack>
-        </Stack>
-    ) : (
-        <></>
+        )
     );
 };
